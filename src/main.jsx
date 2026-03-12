35f574c605237e67968da1052ba77a01c1f1cf6a
sandeshkannur <34433725+sandeshkannur@users.noreply.github.com>
2026-03-12 00:12:55 +0000

    Wrap App component with LanguageProvider

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
+import { LanguageProvider } from "./locales/LanguageProvider.jsx";
 import "./index.css";
 import "./App.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <LanguageProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </LanguageProvider>
   </React.StrictMode>
 );
